65b73aa304ab8406c4341609cc667da4a2aafe31
Rémi GATTAZ <remi.gattaz@gmail.com>
2016-02-03 10:25:52 +0100

    Step2 : Add stdio.h file to handle EOF

--- code/test/getchar.c
+++ code/test/getchar.c
@@ -1,10 +1,14 @@
 #include "syscall.h"
+#include "stdio.h"
 
 int main() {
     char c;
     /* Read a char and print it */
     c = GetChar();
-    PutChar(c);
+    while(c != EOF){
+        PutChar(c);
+        c = GetChar();
+    }
     PutChar('\n');
     Halt();
 }
---
+++ code/test/stdio.h
@@ -0,0 +1,18 @@
+/* stdio.h
+ *	This file is included by user programs
+ *
+ * Copyright (c) 1992-1993 The Regents of the University of California.
+ * All rights reserved.  See copyright.h for copyright notice and limitation
+ * of liability and disclaimer of warranty provisions.
+ */
+
+#ifdef CHANGED
+
+#ifndef STDIO_H
+#define STDIO_H
+
+#define EOF -1
+
+#endif /* STDIO */
+
+#endif /* CHANGED */
