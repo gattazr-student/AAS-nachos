1385756cbbccce115a9a36696e7a5957a3dd413d
Rémi GATTAZ <remi.gattaz@gmail.com>
2016-02-19 08:17:01 +0100

    Creation of UserThreadExit

--- code/test/userThread.c
+++ code/test/userThread.c
@@ -3,16 +3,16 @@
 
 void test(void * arg) {
     PutString("test\n");
-    Exit(0);
+    UserThreadExit();
 }
-   
+
 int main() {
     PutString("Début du main\n");
     UserThreadCreate(test, 0);
-   
+
     char c = GetChar();
     PutChar(c);
     PutString("Fin du main\n");
-   
+
     Halt();
 }
--- code/userprog/userthread.cc
+++ code/userprog/userthread.cc
@@ -5,48 +5,54 @@
 
 
 int do_UserThreadCreate(int f, int arg) {
-    
+
     int new_tId = threadbitmap->Find();
     if (new_tId > 0) {
         Thread* newThread = new Thread ("slave", new_tId);
-        
+
         struct struct_user_thread* args = new (struct struct_user_thread)();
         args->f = f;
         args->arg = arg;
-        
+
         newThread->Fork(StartUserThread, (int)(args));
     }
     else
         return -1;
-    
+
     return 0;
 }
-    
+
 int do_UserThreadExit() {
+    // Clear the bitmap
+    threadbitmap->Clear(currentThread->getId());
+
+    // Kill the thread
+    currentThread->Finish();
+
     return 0;
 }
 /*
 void UserThreadExit() {
-  
+
 }
 */
 void StartUserThread(int f) {
 
     struct_user_thread* args;
     args = (struct_user_thread*) f;
-    
+
     currentThread->space->InitRegisters();
     currentThread->space->RestoreState();
-    
+
     unsigned int regdpl = currentThread->getId()*PageThread*PageSize;
     machine->WriteRegister(StackReg, currentThread->space->getNumPages()*PageSize - regdpl - 16);
-    
+
     machine->WriteRegister(PCReg, args->f);
     machine->WriteRegister(NextPCReg, args->f + 4);
     machine->WriteRegister(4, args->arg);
-    
+
     delete args;
-    
+
     machine->Run();
 }
 
